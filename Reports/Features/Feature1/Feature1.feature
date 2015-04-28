b83c6803912a38d30a709fa0a79056946c616a1b
fraczo <jacek.rawiak@hotmail.com>
2015-04-28 14:24:31 +0200

    publikacja
    Raport wstępny aktualizacji statusuów

--- Reports/Features/Feature1/Feature1.feature
+++ Reports/Features/Feature1/Feature1.feature
@@ -2,5 +2,6 @@
 <feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="09150b32-e6bd-4f24-a7fa-44ec64f37c08" featureId="09150b32-e6bd-4f24-a7fa-44ec64f37c08" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="MasterLeasing Reports Feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
   <projectItems>
     <projectItemReference itemId="96cc3bf8-c9c7-4040-9da6-7c7054b47838" />
+    <projectItemReference itemId="0d13ec0c-53c4-4b3d-b04b-cdab27603cae" />
   </projectItems>
 </feature>
---
+++ Reports/tabKontraktyEventReceiver/tabKontraktyEventReceiver.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.Text;
+
+namespace masterleasing.Reports.tabKontraktyEventReceiver
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class tabKontraktyEventReceiver : SPItemEventReceiver
+    {
+       /// <summary>
+       /// An item was updated.
+       /// </summary>
+       public override void ItemUpdated(SPItemEventProperties properties)
+       {
+           base.ItemUpdated(properties);
+           Execute(properties);
+       }
+
+       private void Execute(SPItemEventProperties properties)
+       {
+           this.EventFiringEnabled = false;
+
+           string status = GetText(properties.ListItem, "colStatusLeadu");
+           int kontraktId = properties.ListItemId;
+           //SPFieldUser user = (SPFieldUser)properties.ListItem["Editor"];
+
+           SPSecurity.RunWithElevatedPrivileges(delegate()
+           {
+               Update_tabStatusTracking(properties, kontraktId, status, DateTime.Today);
+           });
+
+           this.EventFiringEnabled = true;
+       }
+
+private void Update_tabStatusTracking(SPItemEventProperties properties,int kontraktId,string status,DateTime targetDate)
+{
+
+           //sprawdź czy istnieje
+            StringBuilder sb = new StringBuilder(@"<Where><And><And><Eq><FieldRef Name=""colKontraktId"" /><Value Type=""Number"">___KontraktId___</Value></Eq><Eq><FieldRef Name=""colStatusLeadu"" /><Value Type=""Text"">___Status___</Value></Eq></And><Eq><FieldRef Name=""Modified"" /><Value Type=""DateTime"">___TargetDate___</Value></Eq></And></Where>");
+            sb.Replace("___KontraktId___",kontraktId.ToString());
+           sb.Replace("___Status___",status.ToString());
+           sb.Replace("___TargetDate___", targetDate.ToShortDateString());
+
+            SPQuery query = new SPQuery();
+            query.Query = sb.ToString();
+
+            using (SPSite site = new SPSite(properties.SiteId))
+            {
+                using (SPWeb web = site.AllWebs[properties.Web.ID])
+                {
+                    SPList list = web.Lists[@"tabStatusTracking"];
+
+                    SPListItemCollection items = list.GetItems(query);
+                    if (items.Count == 0)
+                    {
+                        SPListItem li = list.AddItem();
+                        li["colKontraktId"] = kontraktId;
+                        li["colStatusLeadu"]= status.ToString();
+                        li["Modified"] = targetDate.ToShortDateString();
+
+                        li.Update();
+                    }
+                }
+            }
+       }
+
+       #region Helpers
+
+private SPUser GetUser_ModifiedBy(SPListItem item)
+{
+    SPFieldUserValue user = (SPFieldUserValue)item["Editor"];
+    return user.User;
+}
+
+//SPUser user = properties.Web.EnsureUser("domain\\username");
+//SPListItem item = properties.Item;
+//item[SPBuiltInFieldId.Modified_x20_By] = user.ID + ";#" + user.loginName;
+//item.SystemUpdate();
+
+       private string GetText(SPListItem sPListItem, string p)
+       {
+           string result = string.Empty;
+
+           if (sPListItem[p] != null)
+           {
+               result = sPListItem[p].ToString();
+           }
+           return result;
+       }
+
+       #endregion
+
+
+
+    }
+}
